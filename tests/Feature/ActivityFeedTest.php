d8a09a068cc4b21b65cc1f8bd38b9ecc9bedfe0c
ramaID <ramaID@users.noreply.github.com>
2022-08-21 05:09:50 +0000

    Fix styling

--- tests/Feature/ActivityFeedTest.php
+++ tests/Feature/ActivityFeedTest.php
@@ -2,9 +2,8 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Facades\Tests\Setup\ProjectFactory;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ActivityFeedTest extends TestCase
